ad73a5b9a80b386e56e7bcae037ef28a68a4b72b
Stephen Gunawan <stephen_gunawan2014@yahoo.com.au>
2025-02-03 16:58:29 +1100

    added ui to the game
    there are now buttons to select and results appear through text

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Rock-Paper-Scissors
-This is a rock paper scissors game using html and javascript. Play five rounds against a computer that generates random choice by typing choice of rock, paper, or scissor in prompt. Result will appear in the console of Developer Tools. The winner of the game will be announced in the console after five rounds. Refresh the page to play again.
+This is a rock paper scissors game using html and javascript. Play first to a score of 5 against a computer that generates random choice by selecting a choice of rock, paper, or scissor. Result will appear through text beneath the buttons. The winner of the game is the first to get a score of 5 and it will be announced throuogh text beneath the buttons. Refresh the page to play again.
--- RPS.js
+++ RPS.js
@@ -1,33 +1,48 @@
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(){
+const button = document.querySelectorAll('button').forEach(button => 
+    button.addEventListener('click', () =>{
+        const finalResult = document.createElement('div');
+        if (humanScore === 5){
+            resultCont.textContent = '';
+            finalResult.textContent = "YOU WIN THE GAMEEEEEEE!!!!!"
+            resultCont.appendChild(finalResult);
+        }else if (computerScore === 5){
+            resultCont.textContent = '';
+            finalResult.textContent = "YOU LOSE THE GAMEEEEEEE!!!!!"
+            resultCont.appendChild(finalResult); 
+        }else{
+            playRound(button.innerHTML);
+        }
+    })
+);
+const resultCont = document.querySelector('.result');
+resultCont.style.paddingTop = '21px';
+function playRound(userChoice){
     let computerChoice = Math.floor(Math.random()*(3-1+1)+1);
-    let userChoice = prompt("Type Rock, Paper or Scissor");
-    if ((computerChoice === 1 && userChoice.toLowerCase() === "rock") || (computerChoice === 2 && userChoice.toLowerCase()==="paper") || (computerChoice=== 3 && userChoice.toLowerCase() === "scissor")){
-        console.log("DRAW!!!");
-    }else if ((computerChoice === 1 && userChoice.toLowerCase()==="paper") || (computerChoice === 2 && userChoice.toLowerCase() ==="scissor") || (computerChoice === 3 && userChoice.toLowerCase() === "rock")){
-        console.log("YOU WINNNNNNN!!!!!!!");
+    const result = document.createElement('div');
+    const UserScore = document.createElement('div');
+    const compScore = document.createElement('div');
+    result.textContent = "Result: "
+    UserScore.textContent = "User Score: "
+    compScore.textContent = "Computer Score: "
+    if ((computerChoice === 1 && userChoice.toString().toLowerCase() === "rock") || (computerChoice === 2 && userChoice.toString().toLowerCase()==="paper") || (computerChoice=== 3 && userChoice.toString().toLowerCase() === "scissor")){
+        result.textContent += "DRAW!!!";
+    }else if ((computerChoice === 1 && userChoice.toString().toLowerCase()==="paper") || (computerChoice === 2 && userChoice.toString().toLowerCase() ==="scissor") || (computerChoice === 3 && userChoice.toString().toLowerCase() === "rock")){
+        result.textContent += "YOU WINNNNNNN!!!!!!!";
         humanScore++;
     }else{
-        console.log("YOU LOSE");
+        result.textContent += "YOU LOSE";
         computerScore++;
     }
+
+    UserScore.textContent += humanScore;
+    compScore.textContent += computerScore;
+    resultCont.textContent = '';
+    resultCont.appendChild(result);
+    resultCont.appendChild(UserScore);
+    resultCont.appendChild(compScore);
 }
 
-function playGame(){
-    for (i = 0; i<5; i++){
-        playRound();
-        console.log("Your Score:" +humanScore);
-        console.log("Computer Score:" +computerScore);
-    }
-    if (humanScore>computerScore){
-        console.log("YOU ARE THE WINNERRRRRRR!!!!!!!");
-    }else if (humanScore<computerScore){
-        console.log("YOU ARE THE LOSEERRRRRRR!!!!!!!"); 
-    }else{
-        console.log("IT IS A DRAWWWWWWWW!!!!!!!"); 
-    }
-}
 
-playGame();
